a467000217b2e60886c12417fea8a41e3419a5bc
Johannes <johannes.launer@lusini.com>
2023-04-11 14:22:36 +0200

    initial component for conditional add div

---
+++ src/app/component/add-task/add-task.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import {Task} from '../../Task'
+import { UiService } from 'src/app/services/ui.service'; 
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-add-task',
+  templateUrl: './add-task.component.html',
+  styleUrls: ['./add-task.component.scss']
+})
+export class AddTaskComponent{
+  text!: string;
+  day!: string;
+  reminder: boolean = false;
+  showAddTask: boolean = false;
+  subscription!: Subscription;
+
+  constructor(private uiService: UiService) { 
+    this.subscription = this.uiService.onToggle().subscribe((value) => (this.showAddTask = value));
+  }
+
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
+
+  onSubmit() {
+    if(!this.text) {
+      alert('Please add a task');
+      return;
+    }
+
+    const newTask = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder
+    }
+
+    console.log(newTask);
+    this.onAddTask.emit(newTask);
+    this.text = '';
+    this.day = '';
+    this.reminder = false;
+  }
+}
